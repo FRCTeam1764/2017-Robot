d6ba1f51cf211be77f9dc0afdb5b05faeb994197
bobbyplubell <bobbyplubell@gmail.com>
2017-02-21 10:59:44 -0600

    MoveGearIntake changed to instantcommand

--- src/org/usfirst/frc/team1764/robot/commands/MoveGearIntake.java
+++ src/org/usfirst/frc/team1764/robot/commands/MoveGearIntake.java
@@ -1,13 +1,12 @@
 package org.usfirst.frc.team1764.robot.commands;
 
 import org.usfirst.frc.team1764.robot.Robot;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
  *
  */
-public class MoveGearIntake extends Command {
-	boolean done = false;
+public class MoveGearIntake extends InstantCommand {
 	boolean state;
 
     public MoveGearIntake(boolean state) {
@@ -19,24 +18,6 @@ public class MoveGearIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
     	Robot.gearIntake.set(state);
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
     }
 }
