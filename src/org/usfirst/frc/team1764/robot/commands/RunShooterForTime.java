734bd02ac97564e54f9b5ce8bd7f74d09599c8e4
bobbyplubell <bobbyplubell@gmail.com>
2017-02-21 19:29:57 -0600

    test commit

--- src/org/usfirst/frc/team1764/robot/commands/RunShooterForTime.java
+++ src/org/usfirst/frc/team1764/robot/commands/RunShooterForTime.java
@@ -23,7 +23,7 @@ public class RunShooterForTime extends TimedCommand {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.shooter.set(Constants.FLYWHEEL_SPEED);
+    	Robot.shooter.set(Constants.FLYWHEEL_SPEED);;
     }
 
     // Called once after timeout
